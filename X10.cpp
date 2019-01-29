d6e6e91b48a7c104b6e9cbcdd01649744aec0f7d
bjarkede <bjarkede@gmail.com>
2019-01-29 21:42:11 +0100

    Intial commit

---
+++ X10.cpp
@@ -0,0 +1,37 @@
+#include "X10.hpp"
+
+void X10_Interface::transmit_code(X10_Code* code) {
+  while(!code->packet.empty()) {
+    // @Incomplete
+    // For each binary code in our data packet: We want to shift off each bit,
+    // and send out the corresponding manchester encoded bit.. What this means is that
+    // for each sine wave, we want to send the bit and its complement on the zero crossing point.
+    // I.E: 0110 -> 0&1 then 1&0 1&0 0&1, for a total of 8 bits. -bjarke, 29th January 2019.
+    for(int i = 0; i <= amount_of_bits(code->packet.front()); ++i) {
+      // 1. Step: Wait for crossing.
+      // 2. Step: Get the bit, and send the first manchester encoded version.
+      // 3. Step: Wait for crossing.
+      // 4. Step: Send the second part of the manchester encoded version.
+    }
+
+    code->packet.pop_front(); // Move onto the next instruction
+  }
+}
+
+bool X10_Interface::wait_for_zero_crossing_point() {
+
+}
+
+// Used for debugging...
+int main(int argc, char* argv[]) {
+  
+  X10_Code* code1 = new X10_Code(HOUSE_A, ON);
+
+  while(!code1->packet.empty()) {
+    std::cout << (int)code1->packet.front() << std::endl;
+    code1->packet.pop_front();
+  }
+
+  return 0;
+}
+
---
+++ X10.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "X10const.hpp"
+
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <deque>
+
+// X10 States
+enum state { IDLE = 0, SENDING = 1, RECEIVING = 2 };
+
+struct X10_Code  {
+  std::deque<char unsigned> packet;
+  X10_Code(char unsigned house_code, char unsigned number_code)
+  {
+    packet.push_back(START_CODE);
+    packet.push_back(house_code);
+    packet.push_back(number_code);
+  }
+};
+
+struct X10_Interface {
+private:
+  int transmission_pin;
+  int receiving_pin;
+
+  state X10_state; 
+protected:
+public:
+  X10_Interface() { X10_state = IDLE;}
+  
+  void transmit_code(X10_Code* code);
+  
+  // @Incomplete
+  // Since X10 transmissions are synchronized to the zero crossing point of the AC power line
+  // we want to be able to see if we hit it, so we can transmit. -bjarke, 29th January 2019
+  bool wait_for_zero_crossing_point();
+};
+
+int amount_of_bits(unsigned char n) {
+  int count = 0;
+  while(n) {
+    count += n % 2;
+    n >>= 1;
+  }
+  return count;
+}
+  
